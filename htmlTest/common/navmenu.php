22426639aa01ac62804278af90a75ef16752f4bd
SahulHameedA <sahul.hameed.a@gmail.com>
2017-01-27 15:48:31 +0530

    htmlTest

---
+++ htmlTest/common/navmenu.php
@@ -0,0 +1,11 @@
+  
+  		<ul class="nav">
+			<li> <a href="" target="main" class="active"> <span class="glyphicon glyphicon-home"></span> HOME </a> </li>
+			<li> <a href="" target="main"> <span class="glyphicon glyphicon-briefcase"></span> CAREER </a> </li>
+			<li> <a href="" target="main"> <span class="glyphicon glyphicon-folder-open"></span> DOCUMENTS </a> </li>
+			<li> <a href="javaScriptSample.html" target="main"> <span class="glyphicon glyphicon-education"></span> EDUCATION </a> </li>
+			<li> <a href="table.html" target="main"> <span class="glyphicon glyphicon-usd"></span> FINANCE </a> </li>
+			<li> <a href="" target="main"> <span class="glyphicon glyphicon-eye-open"></span> ISLAM </a> </li>
+			<li style="float:right"><a href=""><span class="glyphicon glyphicon-cog"></span> SETTINGS </a></li>
+		</ul>
+		
